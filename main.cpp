80e38666165e0d3297c8190a2202127de7d78c1c
DashCampbell <90148639+DashCampbell@users.noreply.github.com>
2022-12-27 19:43:57 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include <vector>
 
-enum nodeType {
+enum nodeType
+{
 	PATH,
 	WALL,
 	TELEPORTER
-}
+};
 
 struct sNode
 {
 	int x, y;
-	vector<*sNode> neighbours;
+	std::vector<*sNode> neighbours;
+	std::vector<*sNode> pathNeighbours;
 	nodeType type;
 };
 
@@ -22,10 +25,53 @@ public:
 		sAppName = "Game";
 	}
 
+private:
+	int mapWidth = 16;
+	int mapHeight = 16;
+	sNode nodeMap[mapHeight][mapWidth];
+
 public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+		// Inialize Map
+		int bitMap[mapHeight][mapWidth] = {
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		};
+		for (int row = 0; row < mapHeight; row++)
+		{
+			for (int col = 0; col < mapWidth; col++)
+			{
+				nodeMap[row][col].x = col;
+				nodeMap[row][col].y = row;
+				switch (bitMap[row][col])
+				{
+				case 1:
+					nodeMap[row][col].type = WALL;
+					break;
+				case 2:
+					nodeMap[row][col].type = TELEPORTER;
+					break;
+				default:
+					nodeMap[row][col].type = PATH;
+				}
+			}
+		}
 		return true;
 	}
 
